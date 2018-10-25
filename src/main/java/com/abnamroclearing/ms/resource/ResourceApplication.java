b924d56d9fc6d48377fc72f97d4ab2df51c37ceb
Rushikesh Teli <Rushikesh.Teli@au.abnamroclearing.com>
2018-10-25 16:03:21 +1100

    Initial Commit

---
+++ src/main/java/com/abnamroclearing/ms/resource/ResourceApplication.java
@@ -0,0 +1,44 @@
+package com.abnamroclearing.ms.resource;
+
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class ResourceApplication {
+
+	@RequestMapping("/")
+	public Message home() {
+		return new Message("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceApplication.class, args);
+	}
+}
+
+class Message {
+	private String id = UUID.randomUUID().toString();
+	private String content;
+
+	Message() {
+	}
+
+	public Message(String content) {
+		this.content = content;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
